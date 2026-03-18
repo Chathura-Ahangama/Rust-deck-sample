e7f940510ea32849d7ce14a6224eaadff269850a
Chathura-Ahangama <chathuraahangama@gmail.com>
2026-03-18 07:54:25 +0530

    simple game-done

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,51 @@
-fn main(){
+use rand::seq::SliceRandom;
 
- print!("Hello, world!");
+
+
+#[derive(Debug)]
+struct Deck{
+    cards:Vec<String>,
+}
+
+impl Deck{
+    fn new() -> Deck{
+
+        let suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+        
+
+        let ranks = [
+            "Ace", "2", "3", "4", "5", "6", "7", 
+            "8", "9", "10", "Jack", "Queen", "King"
+        ];
+        let mut pack = vec![];
+
+        for suit in suits{
+            for rank in ranks{
+               let card= format!("{} of {}",rank,suit);
+               pack.push(card);
+            }
+        }
+
+        Deck{cards:pack}
+    }
+
+    fn shuffle(&mut self) {
+        let mut rnd = rand::rng();
+        self.cards.shuffle(&mut rnd);
+    }
+
+    fn deal(&mut self,num:usize) -> Vec<String>{
+        self.cards.split_off(self.cards.len()-num)
+    }
+}
+
+fn main(){
+ let mut result = Deck::new();
+
+ result.shuffle();
+ 
+ let result2 = result.deal(5);
+ println!("{:#?}",result);
+ println!("{:#?}",result2);
 
 }
